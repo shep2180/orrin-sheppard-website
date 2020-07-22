c007d60a192ed8ea32d4576e972f5420ef7ed678
Orrin Sheppard <shep2180@gmail.com>
2020-07-22 07:54:24 -0500

    Cleaning up script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 console.log("Editing visitor count");
 
 fetch('https://rxxr3ccmuj.execute-api.us-east-1.amazonaws.com/Staging/testsamapi')
-	//.then(response => response.json())
-	//.then(data => console.log(data))
 	.then(function(response)
 	{
 		return response.json()
